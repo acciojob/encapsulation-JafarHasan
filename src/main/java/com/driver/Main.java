ab68881daa2cbad958e752c2e2e78741308af8c6
jafar <se.hasanjafar@gmail.com>
2024-04-13 11:04:20 +0530

    getter and setter() added.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,14 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
       RWOnly obj=new RWOnly();
-      obj.no=120;
-      System.out.println(obj.no);
+      /*obj.no=120;
+      System.out.println(obj.no);*/
+      /* Error:  COMPILATION ERROR :
+[INFO] -------------------------------------------------------------
+Error:  /home/runner/work/encapsulation-JafarHasan/encapsulation-JafarHasan/src/main/java/com/driver/Main.java:[6,10] no has private access in com.driver.RWOnly
+Error:  /home/runner/work/encapsulation-JafarHasan/encapsulation-JafarHasan/src/main/java/com/driver/Main.java:[7,29] no has private access in com.driver.RWOnly*/
+      obj.setNo(120);
+      System.out.println(obj.getNo());
+
   }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,13 @@ package com.driver;
 
 public class RWOnly {
     private int no;
+
+    //getter:
+    public int getNo(){
+        return this.no;
+    }
+    //setter
+    public void setNo(int No){
+        this.no=No;
+    }
 }
