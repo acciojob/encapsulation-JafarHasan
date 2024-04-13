e82f6f5581bd108ecd84e0b6dcf1c1383abe900e
jafar <se.hasanjafar@gmail.com>
2024-04-13 10:55:29 +0530

    Submit!

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj=new RWOnly();
+      obj.no=120;
+      System.out.println(obj.no);
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,5 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int no;
 }
